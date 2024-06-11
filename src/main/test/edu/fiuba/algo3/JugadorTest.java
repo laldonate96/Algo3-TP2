e72868811155f5410840923c9b8d6004f5df0cb7
nachoorgeira <nachoorgeira@gmail.com>
2024-06-11 17:17:28 -0300

    Changes by lucas

--- src/main/test/edu/fiuba/algo3/JugadorTest.java
+++ src/main/test/edu/fiuba/algo3/JugadorTest.java
@@ -3,6 +3,9 @@ package edu.fiuba.algo3;
 import edu.fiuba.algo3.modelo.jugador.Jugador;
 import edu.fiuba.algo3.modelo.modificador.Modificador;
 import edu.fiuba.algo3.modelo.modificador.Multiplicador;
+import edu.fiuba.algo3.modelo.opcion.Simple;
+import edu.fiuba.algo3.modelo.opcion.Opcion;
+import edu.fiuba.algo3.modelo.estado.Incorrecta;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,12 +18,17 @@ public class JugadorTest {
     private Jugador jugador;
     private Modificador modificador;
     private List<Modificador> modificadores;
+    private List<Opcion> opciones;
+    private Opcion opcion;
 
     @BeforeEach
     public void setUpClass() {
         modificador = new Multiplicador(2);
         modificadores = new ArrayList<>();
         modificadores.add(modificador);
+        opcion = new Simple("Opcion 1", new Incorrecta());
+        opciones = new ArrayList<>();
+        opciones.add(opcion);
 
         jugador = new Jugador("Jugador 1", modificadores);
     }
@@ -32,22 +40,12 @@ public class JugadorTest {
         assertEquals(2, jugador.obtenerPuntaje());
     }
 
-@Test
-public void test01UsarUnModificadorNoNuloRemueveDeLaListaDeModificadoresDelJugador() {
+    @Test
+    public void test02UsarUnModificadorNoNuloRemueveDeLaListaDeModificadoresDelJugador() {
+        jugador.responder(opciones, opciones, modificador);
 
-//        assertEquals(2, modificadores.size());
-}
+        assertEquals(0, modificadores.size());
+    }
 
-@Test
-public void test02UsarUnModificadorNuloNoRemueveDeLaListaDeModificadoresDelJugador() {
-
-//        assertEquals(3, modificadores.size());
-}
-
-@Test
-public void test03UsarUnMultiplicadorNoTieneMasUsosLuegoDeUsarseUnaVez() {
-
-//        assertEquals(false, modificador1.tieneUsos());
-}
 }
 
