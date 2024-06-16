b9953a3d8cf442d6ea209b8e10dfc3cddb4494bb
nachoorgeira <nachoorgeira@gmail.com>
2024-06-16 20:36:31 -0300

    Esqueleto clase Juego (fachada)

---
+++ src/main/java/edu/fiuba/algo3/modelo/juego/Juego.java
@@ -0,0 +1,36 @@
+package edu.fiuba.algo3.modelo.juego;
+
+import edu.fiuba.algo3.modelo.pregunta.Pregunta;
+import edu.fiuba.algo3.modelo.turno.Turno;
+import edu.fiuba.algo3.modelo.jugador.Jugador;
+
+import java.util.List;
+
+public class Juego {
+
+    private Turno turnoActual;
+    private List<Pregunta> preguntas;
+    private List<Jugador> jugadores;
+
+    public void IniciarJuego(){
+
+        //Controlador.InicializarJuego()
+        //muestra el comienzo del juego, solicita ingreso de jugadores y los va agregando a jugadores
+
+        //Lector.ObtenerPreguntasDeJson()
+        //carga todas las preguntas en preguntas
+
+        int cantidadDePreguntas = preguntas.size();
+        int i = 0;
+        while (i < cantidadDePreguntas) {
+
+            //turnoActual = Turno new(jugadores, preguntas[i])
+            //para cada pregunta inicializa un turno, pasandole los jugadores y la pregunta.
+
+            //Controlador.MostrarTurno(Turno);
+            //la lógica asociada al turno
+            i++;
+        }
+        return;
+    }
+}
