13272711eac79540d9e52cf44847a8ccadaaba96
valentino ceniceros <vceniceros2001@gmail.com>
2024-06-17 17:31:11 -0300

    avances en vista

---
+++ src/main/java/edu/fiuba/algo3/vista/PreguntaLayout.java
@@ -0,0 +1,113 @@
+package edu.fiuba.algo3.vista;
+
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class PreguntaLayout extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Formulario de Pregunta");
+
+        Label preguntaLabel = new Label("Pregunta");
+        preguntaLabel.getStyleClass().add("titulo-label");
+
+        Label jugadorLabel = new Label("Jugador:");
+        TextField jugadorTextField = new TextField();
+        jugadorTextField.setMaxWidth(200);
+
+        Label turnoLabel = new Label("Turno:");
+        TextField turnoTextField = new TextField();
+        turnoTextField.setMaxWidth(50);
+
+        Label puntosLabel = new Label("Puntos");
+        TextField puntosTextField = new TextField();
+        puntosTextField.setMaxWidth(50);
+
+        Label modificadorLabel = new Label("Modificador jugado");
+        TextField modificadorTextField = new TextField();
+        modificadorTextField.setMaxWidth(50);
+
+        //if(penalidad){
+        Label penalidadLabel = new Label("Penalidad:");
+        TextField penalidadTextField = new TextField();
+        penalidadTextField.setMaxWidth(50);
+        //}
+
+        TextArea preguntaTextArea = new TextArea();
+        preguntaTextArea.setEditable(false);
+        preguntaTextArea.setWrapText(true);
+        preguntaTextArea.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                + "Nullam suscipit est ut purus consequat, ac scelerisque justo varius. "
+                + "Vivamus ac ante in ipsum commodo viverra nec in ligula.");
+
+        CheckBox opcion1CheckBox = new CheckBox("Opción 1");
+        opcion1CheckBox.getStyleClass().add("checkbox");
+        
+
+        CheckBox opcion2CheckBox = new CheckBox("Opción 2");
+        opcion2CheckBox.getStyleClass().add("checkbox");
+       
+
+        Button enviarRespuestaButton = new Button("Enviar Respuesta");
+        enviarRespuestaButton.getStyleClass().add("enviar-button");
+
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+
+        
+
+        gridPane.add(turnoLabel, 0, 0);
+        gridPane.add(turnoTextField, 1, 0);
+        
+        gridPane.add(jugadorLabel, 0, 1);
+        gridPane.add(jugadorTextField, 1, 1);
+        
+        gridPane.add(puntosLabel, 0, 2);
+        gridPane.add(puntosTextField, 1, 2);
+
+        gridPane.add(modificadorLabel, 0, 3);
+        gridPane.add(modificadorTextField, 1, 3);
+
+      /*  gridPane.add(penalidadLabel, 0, 4);
+        gridPane.add(penalidadTextField, 1, 4);*/ 
+
+        gridPane.add(preguntaLabel, 0, 5, 2, 1);
+        gridPane.add(preguntaTextArea, 0, 6, 2, 1);
+        
+
+        //if(x tipo de pregunta){
+        gridPane.add(opcion1CheckBox, 0, 7);
+        gridPane.add(opcion2CheckBox, 0, 8);
+        
+        gridPane.add(enviarRespuestaButton, 0, 9, 2, 1);
+        
+        GridPane.setHalignment(enviarRespuestaButton, javafx.geometry.HPos.CENTER);
+
+        //}
+        gridPane.getStyleClass().add("form-grid");
+
+        Scene scene = new Scene(gridPane, 450, 500);
+        String css = this.getClass().getResource("src/css/style.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/edu/fiuba/algo3/vista/inicioDelJuego.java
@@ -0,0 +1,47 @@
+package edu.fiuba.algo3.vista;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class inicioDelJuego extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Bienvenido a algo kahoot");
+
+        Label label = new Label("Agregar Jugador:");
+        label.getStyleClass().add("label-style");
+        
+        TextField inputJugador = new TextField();
+        TextArea jugadoresGuardados = new TextArea();
+        jugadoresGuardados.setEditable(false); 
+
+        Button botonJugar = new Button();
+        botonJugar.setText("Jugar");
+        
+        
+        VBox layout = new VBox(10); 
+        layout.setPadding(new Insets(20)); 
+        layout.getChildren().addAll(label, inputJugador, jugadoresGuardados, botonJugar);
+        layout.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(layout, 400, 350);
+        stage.setScene(scene);
+
+
+        String css = this.getClass().getResource("src/css/style.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        stage.show();
+    }
+}
