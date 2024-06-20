87c734c1fb643a877b49833e2ebb1cce8611042a
nachoorgeira <nachoorgeira@gmail.com>
2024-06-20 16:42:35 -0300

    fixed penalidad errorcito

--- src/main/java/edu/fiuba/algo3/modelo/puntaje/ConPenalidad.java
+++ src/main/java/edu/fiuba/algo3/modelo/puntaje/ConPenalidad.java
@@ -5,11 +5,15 @@ import edu.fiuba.algo3.modelo.Respuesta.Respuesta;
 
 public class ConPenalidad extends Puntaje {
     protected int puntajeIncorrecta=-1;
+    private int correctas = 0;
+    private int incorrectas = 0;
 
     @Override
     public void asignarPuntaje(Respuesta respuesta) {
         for (Opcion opcion : respuesta.obtenerOpciones()) {
-            respuesta.asignarPuntaje(opcion.contarCorrecta()*puntaje + opcion.contarIncorrecta()*puntajeIncorrecta);
+            correctas += opcion.contarCorrecta();
+            incorrectas += opcion.contarIncorrecta();
         }
+        respuesta.asignarPuntaje(correctas*puntaje + incorrectas*puntajeIncorrecta);
     }
 }
