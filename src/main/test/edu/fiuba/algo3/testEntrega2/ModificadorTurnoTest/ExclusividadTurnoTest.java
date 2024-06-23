05790860968219d1e0d4ea9fef6837cc2076c494
nachoorgeira <nachoorgeira@gmail.com>
2024-06-23 20:24:40 -0300

    Listo tests exclusividad turno (:

--- src/main/test/edu/fiuba/algo3/testEntrega2/ModificadorTurnoTest/ExclusividadTurnoTest.java
+++ src/main/test/edu/fiuba/algo3/testEntrega2/ModificadorTurnoTest/ExclusividadTurnoTest.java
@@ -40,6 +40,7 @@ public class ExclusividadTurnoTest {
         modificadorPuntaje = new ExclusividadPuntaje();
         modificadores = new ArrayList<>();
         modificadores.add(modificadorPuntaje);
+        modificadores.add(new Multiplicador(2));
 
         jugador = new Jugador("Jugador1", modificadores);
 
@@ -63,7 +64,7 @@ public class ExclusividadTurnoTest {
     @Test
     public void test01UnJugadorRespodeBienYSeDuplicaElPuntajeDeSuRespuesta() {
         //Arrange
-        exclusividad.usar(modificadorPuntaje, jugador2);
+        exclusividad.usar(modificadorPuntaje, jugador);
 
         //Act
         exclusividad.modificarPuntajes(respuestas);
@@ -100,25 +101,19 @@ public class ExclusividadTurnoTest {
     @Test
     public void test04MultiplesLlamadosSinAsignarExclusividadNoMultiplicanElEfecto() {
         //Arrange
-        modificadorPuntaje = mock(AnuladorPuntaje.class);
-        exclusividad.usar(modificadorPuntaje, jugador);
-
-        Jugador jugador2 = mock(Jugador.class);
-        ModificadorPuntaje multiplicador = mock(Multiplicador.class);
-        exclusividad.usar(multiplicador, jugador2);
+        exclusividad.usar(new Multiplicador(2), jugador);
+        exclusividad.usar(modificadorPuntaje, jugador2);
 
         //Act
-
         exclusividad.modificarPuntajes(respuestas);
 
-
         //Assert
-        //assertEquals(28, respuestaMock.obtenerPuntaje());
+        assertEquals(20, respuesta.obtenerPuntaje());
     }
 
     @Test
 
-    public void test04RecibeDosRespuestasCorrectasYAnulaLosPuntosDeAmbas() {
+    public void test05RecibeDosRespuestasCorrectasYAnulaLosPuntosDeAmbas() {
         //Arrange
         exclusividad.usar(modificadorPuntaje, jugador);
 
@@ -135,47 +130,30 @@ public class ExclusividadTurnoTest {
     }
 
     @Test
-    public void test05AUnaRespuestaCorrectaDeUnJugadorQueNoActivoExclusividadSeLeDuplicaElPuntaje() {
+    public void test06AUnaRespuestaCorrectaDeUnJugadorQueNoActivoExclusividadSeLeDuplicaElPuntaje() {
         //Arrange
-        modificadorPuntaje = mock(AnuladorPuntaje.class);
-        exclusividad.usar(modificadorPuntaje, jugador);
-        //respuestaMock.asignarPuntaje(0);
-
-        Jugador jugador2 = mock(Jugador.class);
-
-        Respuesta respuestaMock2 = mock(Respuesta.class);
-        when(respuestaMock2.perteneceA(jugador2)).thenReturn(true);
-        respuestaMock2.asignarPuntaje(13);
+        exclusividad.usar(modificadorPuntaje, jugador2);
 
         //Act
         exclusividad.modificarPuntajes(respuestas);
 
         //Assert
-        assertEquals(26, respuestaMock2.obtenerPuntaje());
-
+        assertEquals(20, respuesta.obtenerPuntaje());
     }
 
     @Test
     public void test06RecibeMultiplesIncorrectasYUnaCorrectaALaCualLeMultiplicaElPuntaje() {
-        modificadorPuntaje = mock(AnuladorPuntaje.class);
         exclusividad.usar(modificadorPuntaje, jugador);
 
-
-        Respuesta respuestaMock2 = mock(Respuesta.class);
-        respuestaMock2.asignarPuntaje(0);
-
-
-        Respuesta respuestaMock3 = mock(Respuesta.class);
-        respuestaMock3.asignarPuntaje(0);
+        Respuesta respuesta3 = new Respuesta(opcionesCorrectas, new Jugador("carlos", modificadores), modificadorPuntaje);
+        respuesta3.asignarPuntaje(0);
 
         //Act
         exclusividad.modificarPuntajes(respuestas);
 
         //Assert
-        //assertEquals(28, respuestaMock.obtenerPuntaje());
-        //assertEquals(0, respuestaMock2.obtenerPuntaje());
-        //assertEquals(0, respuestaMock3.obtenerPuntaje());
-
-
+        assertEquals(20, respuesta.obtenerPuntaje());
+        assertEquals(0, respuesta2.obtenerPuntaje());
+        assertEquals(0, respuesta3.obtenerPuntaje());
     }
 }
