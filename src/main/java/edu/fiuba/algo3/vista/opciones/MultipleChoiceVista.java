df285ac75440a75a5dbfd97b3dd5081f5633706e
nachoorgeira <nachoorgeira@gmail.com>
2024-06-24 02:00:16 -0300

    multiple choice vista

---
+++ src/main/java/edu/fiuba/algo3/vista/opciones/MultipleChoiceVista.java
@@ -0,0 +1,38 @@
+package edu.fiuba.algo3.vista.opciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.fiuba.algo3.modelo.pregunta.Pregunta;
+import edu.fiuba.algo3.modelo.opcion.Opcion;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class MultipleChoiceVista implements OpcionesVista {
+    private List<CheckBox> checkBoxsOpciones;
+    private List<Opcion> opciones;
+
+    @Override
+    public List<Opcion> retornarOpcionesDelJugador() {
+        List<Opcion> opcionesSeleccionadas = new ArrayList<>();
+        for(int i = 0; i < opciones.size(); i++){
+            if(checkBoxsOpciones.get(i).isSelected()){
+                opcionesSeleccionadas.add(opciones.get(i));
+            }
+        }
+        return opcionesSeleccionadas;
+    }
+
+    @Override
+    public void mostrarOpciones(List<Opcion> opcionesRecibidas, Pane contenedor) {
+        opciones = opcionesRecibidas;
+        checkBoxsOpciones = new ArrayList<>();
+        for(Opcion opcion : opcionesRecibidas){
+            CheckBox checkBoxOpcion = new CheckBox(opcion.obtenerTexto());
+            checkBoxsOpciones.add(checkBoxOpcion);
+            contenedor.getChildren().addAll(checkBoxOpcion);
+        }
+    }
+}
