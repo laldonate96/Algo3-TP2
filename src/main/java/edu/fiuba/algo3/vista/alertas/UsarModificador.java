45b4c75dd28eaea79cd0276d7767e48e1da26cf9
valentino ceniceros <vceniceros2001@gmail.com>
2024-06-29 18:27:46 -0300

    creado alerta de modificador

---
+++ src/main/java/edu/fiuba/algo3/vista/alertas/UsarModificador.java
@@ -0,0 +1,9 @@
+package edu.fiuba.algo3.vista.alertas;
+
+import javafx.scene.control.Alert;
+
+public class UsarModificador extends Alerta{
+    public UsarModificador(){
+        super("Esta seguro de que quiere usar el modificador", "tiene un unico uso para cada modificador");
+    }
+}
