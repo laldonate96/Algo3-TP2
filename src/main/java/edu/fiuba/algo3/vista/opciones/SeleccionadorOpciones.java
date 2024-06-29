5ab0448033deac142ba91e45250596a1e04d742c
valentino ceniceros <vceniceros2001@gmail.com>
2024-06-29 17:12:02 -0300

    fix a obtenerPreguntaOpciones

--- src/main/java/edu/fiuba/algo3/vista/opciones/SeleccionadorOpciones.java
+++ src/main/java/edu/fiuba/algo3/vista/opciones/SeleccionadorOpciones.java
@@ -1,33 +1,30 @@
 package edu.fiuba.algo3.vista.opciones;
 
-import java.util.List;
-
-import edu.fiuba.algo3.modelo.opcion.Opcion;
 import edu.fiuba.algo3.modelo.pregunta.Pregunta;
 import edu.fiuba.algo3.vista.alertas.Alerta;
 import edu.fiuba.algo3.vista.alertas.AlgoSalioMal;
 import javafx.scene.layout.GridPane;
 
 public class SeleccionadorOpciones {
-    public static void seleccionarVistaOpciones(List<Opcion> opciones, Pregunta pregunta, GridPane pane){
+    public static void seleccionarVistaOpciones(Pregunta pregunta, GridPane pane){
         String preguntaString = pregunta.getClass().toString();
         String tipoPregunta = preguntaString.substring(preguntaString.lastIndexOf('.') + 1);
         switch (tipoPregunta) {
             case "VerdaderoFalso":
                 VerdaderoOFalsoVista verdaderoOFalsoVista = new VerdaderoOFalsoVista();
-                verdaderoOFalsoVista.mostrarOpciones(opciones, pane);
+                verdaderoOFalsoVista.mostrarOpciones(pregunta.obtenerOpciones(), pane);
                 break;
             case "MultipleChoice":
                 MultipleChoiceVista multipleChoiceVista = new MultipleChoiceVista();
-                multipleChoiceVista.mostrarOpciones(opciones, pane);
+                multipleChoiceVista.mostrarOpciones(pregunta.obtenerOpciones(), pane);
                 break;
             case "OrderedChoice":
                 OrderedChoiceVista orderedChoiceVista = new OrderedChoiceVista();
-                orderedChoiceVista.mostrarOpciones(opciones, pane);
+                orderedChoiceVista.mostrarOpciones(pregunta.obtenerOpciones(), pane);
                 break;
             case "GroupChoice":
                 EleccionGrupalVista eleccionGrupalVista = new EleccionGrupalVista();
-                eleccionGrupalVista.mostrarOpciones(opciones, pane);    
+                eleccionGrupalVista.mostrarOpciones(pregunta.obtenerOpciones(), pane);    
                 break;
             default:
                 Alerta alerta = new AlgoSalioMal();
