cd9bd1cec94fdba62cd8fe770a38939f20ab268d
valentino ceniceros <vceniceros2001@gmail.com>
2024-06-30 15:35:16 -0300

    refactor a botonEnviarRespuesta

--- src/main/java/edu/fiuba/algo3/vista/botones/BotonEnviarRespuesta.java
+++ src/main/java/edu/fiuba/algo3/vista/botones/BotonEnviarRespuesta.java
@@ -1,32 +1,18 @@
 package edu.fiuba.algo3.vista.botones;
 
+import java.util.List;
+
 import edu.fiuba.algo3.controlador.ControladorDeTurno;
 import edu.fiuba.algo3.modelo.Modificador.Modificador;
 import edu.fiuba.algo3.modelo.opcion.Opcion;
 import edu.fiuba.algo3.modelo.turno.Turno;
-import javafx.animation.Animation;
-import javafx.animation.FadeTransition;
-import javafx.scene.Cursor;
-import javafx.scene.control.Button;
-import javafx.util.Duration;
 
-import java.util.List;
-
-public class BotonEnviarRespuesta extends Button {
+public class BotonEnviarRespuesta extends Boton {
 
 
     public BotonEnviarRespuesta(Turno turno, List<Opcion> opcionesElegidas, Modificador modificador) {
-        super();
-        this.setText("Enviar");
-        this.setMinHeight(30);
-        FadeTransition titilante = new FadeTransition(Duration.seconds(1), this);
-        titilante.setFromValue(1.0);
-        titilante.setToValue(0.0);
-        titilante.setCycleCount(Animation.INDEFINITE);
-        titilante.setAutoReverse(true);
-        titilante.play();
-        this.setOnMouseEntered(event -> this.setCursor(Cursor.HAND));
-        this.setOnMouseExited(event -> this.setCursor(Cursor.DEFAULT));
+        super("Enviar", "button");
+        this.setMinHeight(30); 
         this.setOnAction(new ControladorDeTurno(turno, opcionesElegidas, modificador));
     }
 }
