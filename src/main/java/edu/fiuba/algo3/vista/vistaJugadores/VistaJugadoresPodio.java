454061f1e2a5edcc6aae1f86dc09806e3e85c010
IvanFuschetto <ifuschetto@fi.uba.ar>
2024-07-02 15:33:37 -0300

    mostrar top5

--- src/main/java/edu/fiuba/algo3/vista/vistaJugadores/VistaJugadoresPodio.java
+++ src/main/java/edu/fiuba/algo3/vista/vistaJugadores/VistaJugadoresPodio.java
@@ -23,30 +23,32 @@ public class VistaJugadoresPodio {
 
         int top = 1;
         for (Jugador jugador : jugadores) {
-            Text jugadorTop1 = new Text(jugador.obtenerNombre() + " ");
-            jugadorTop1.getStyleClass().add("primeraVentanaTitulo");
-            VBox jugadorBox = new VBox(10);
-            jugadorBox.getChildren().addAll(jugadorTop1);
+            if (top < 6) {
+                Text jugadorTop1 = new Text(jugador.obtenerNombre() + " ");
+                jugadorTop1.getStyleClass().add("primeraVentanaTitulo");
+                VBox jugadorBox = new VBox(10);
+                jugadorBox.getChildren().addAll(jugadorTop1);
 
-            Text puntuacion = new Text(jugador.obtenerPuntaje() + " puntos");
-            puntuacion.getStyleClass().add("primeraVentanaTitulo");
-            VBox puntuacionBox = new VBox(10);
-            puntuacionBox.getChildren().addAll(puntuacion);
+                Text puntuacion = new Text(jugador.obtenerPuntaje() + " puntos");
+                puntuacion.getStyleClass().add("primeraVentanaTitulo");
+                VBox puntuacionBox = new VBox(10);
+                puntuacionBox.getChildren().addAll(puntuacion);
 
-            if (top == 1) {
-                jugadorBox.getStyleClass().add("jugadorGanador");
-                puntuacionBox.getStyleClass().add("puntuacionGanador");
-            } else {
-                jugadorBox.getStyleClass().add("jugadoresPodio");
-                puntuacionBox.getStyleClass().add("jugadoresPodio");
-                ;
+                if (top == 1) {
+                    jugadorBox.getStyleClass().add("jugadorGanador");
+                    puntuacionBox.getStyleClass().add("puntuacionGanador");
+                } else {
+                    jugadorBox.getStyleClass().add("jugadoresPodio");
+                    puntuacionBox.getStyleClass().add("jugadoresPodio");
+                    ;
+                }
+
+                HBox contenedor2 = new HBox(10);
+                contenedor2.getChildren().addAll(jugadorBox, puntuacionBox);
+                contenedor2.setAlignment(Pos.CENTER);
+                jugadoresPodio.getChildren().add(contenedor2);
+                top++;
             }
-
-            HBox contenedor2 = new HBox(10);
-            contenedor2.getChildren().addAll(jugadorBox, puntuacionBox);
-            contenedor2.setAlignment(Pos.CENTER);
-            jugadoresPodio.getChildren().add(contenedor2);
-            top++;
         }
     }
 }
