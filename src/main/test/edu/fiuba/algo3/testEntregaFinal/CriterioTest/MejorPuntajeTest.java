7d92343d88931b48ae8260900cc410264d5bccd4
laldonate96 <lucasaldonate@gmail.com>
2024-07-01 21:57:27 -0300

    Fix a error en el build

--- src/main/test/edu/fiuba/algo3/testEntregaFinal/CriterioTest/MejorPuntajeTest.java
+++ src/main/test/edu/fiuba/algo3/testEntregaFinal/CriterioTest/MejorPuntajeTest.java
@@ -1,13 +1,16 @@
 package edu.fiuba.algo3.testEntregaFinal.CriterioTest;
 
-import edu.fiuba.algo3.modelo.criterioDeVictoria.MejorPuntaje;
-import edu.fiuba.algo3.modelo.jugador.Jugador;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.fiuba.algo3.modelo.criterioDeVictoria.MejorPuntaje;
+import edu.fiuba.algo3.modelo.jugador.Jugador;
 
 public class MejorPuntajeTest {
 
