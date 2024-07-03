f473744334848a7f976153a6beec46b72f5abcbc
nachoorgeira <nachoorgeira@gmail.com>
2024-07-02 21:51:54 -0300

    diagramas actualizados y volar pregunta inexistente exception

--- src/main/java/edu/fiuba/algo3/modelo/excepciones/PreguntaInexistenteException.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3.modelo.excepciones;
-
-public class PreguntaInexistenteException extends RuntimeException {
-    public PreguntaInexistenteException(String mensaje){
-        super(mensaje);
-    }
-}
